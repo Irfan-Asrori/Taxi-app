ea4d2c555540b65bcfda7b34631f96a2acaeb3d5
Irfan Asrori <irfanasrori@students.amikom.ac.id>
2021-03-17 20:13:48 +0700

    commit taxi

---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty: yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty: no");
+            }
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+            Console.WriteLine();
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropoffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
